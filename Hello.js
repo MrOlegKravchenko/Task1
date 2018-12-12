d84b9200af3c0c7ab4d4fa6bc03d366455afc7ee
Oleg Kravchenko <mr.oleg.kravchenko@mail.ru>
2018-12-12 21:54:51 +0300

    some changes

--- Hello.js
+++ Hello.js
@@ -1,2 +1,8 @@
-alert("I'm watching you");
-alert('motherf*cker');
+var name = 'oleg';
+var obj= {
+    name:'asd',
+    secondName:'asd'
+}
+alert("I'm watching you"+ obj);
+name = 'alex';
+alert('motherf*cker'+name);
