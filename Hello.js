03ba4da3dbc602025d328cabc4d05f2c1d67ed7c
AlexeyDavydov <alexeika.ya@mail.ru>
2018-12-15 17:44:47 +0300

    fixed text copy

--- Hello.js
+++ Hello.js
@@ -1,5 +1,33 @@
-var name = 'oleg';
-alert(name+' motherf*cker');
-function Fx(){
-    document.getElementById("txtCopy").innerHTML= document.getElementById("txtPaste")
+// var name = 'oleg';
+// // alert(name+' motherf*cker');
+// var Fx = (event) => {
+//     event.preventDefault();
+//     console.log(event)
+//     var txt =  document.getElementById("txtCopy").value;
+//     console.log(txt)
+//     document.getElementById("txtPaste").innerText = txt ;
+// }
+
+// var btn = document.getElementById('btn');
+// btn.addEventListener('click', Fx(event));
+
+var btn = document.getElementById('btn');
+// btn.onclick = function(e){
+//     e.preventDefault();
+//     alert('asd')
+//     var txt =  document.getElementById("txtCopy").value;
+//     console.log(txt)
+//     document.getElementById("txtPaste").innerText = txt ;
+// }
+
+
+function fx(event){
+    event.preventDefault();
+    console.log(event)
+    var txt =  document.getElementById("txtCopy").value;
+    console.log(txt)
+    document.getElementById("txtPaste").innerText = txt ;
 }
+
+
+btn.addEventListener('click', fx.bind(this));
