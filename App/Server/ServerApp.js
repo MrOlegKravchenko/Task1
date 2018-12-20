87d7a74c38274a5564ab5cbe6974d7f126253b5e
Oleg Kravchenko <mr.oleg.kravchenko@mail.ru>
2018-12-20 23:08:26 +0300

    modified: App/Server/ServerApp.js and  package-lock.json

--- App/Server/ServerApp.js
+++ App/Server/ServerApp.js
@@ -1,7 +1,13 @@
 let Express = require('express');
+let path = require('path');
 let app = Express();
 
 const port = 8080;
+
+app.get('/oleg',function(req,res){
+    res.sendFile(path.join(__dirname,'../Client/public/index.html'));
+    // res.send('Oleg loool');
+});
 
 app.listen(port, ()=>{
     console.log('server is up now. Check out port ' + port);
