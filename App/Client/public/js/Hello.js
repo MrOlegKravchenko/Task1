793602eb9d3d367b7425d6751373fe3cd1d85fe1
Aleh Krauchanka <AKrauchanka@gomel.iba.by>
2019-01-23 23:18:55 +0300

    deleted Index page, clean all old comments from Hello.js and ServerApp, made buttonSave workes

--- App/Client/public/js/Hello.js
+++ App/Client/public/js/Hello.js
@@ -1,5 +1,3 @@
-// var name = 'oleg';
-// // alert(name+' motherf*cker');
 // var Fx = (event) => {
 //     event.preventDefault();
 //     console.log(event)
@@ -7,19 +5,10 @@
 //     console.log(txt)
 //     document.getElementById("txtPaste").innerText = txt ;
 // }
-
 // var btn = document.getElementById('btn');
 // btn.addEventListener('click', Fx(event));
 
 var btn = document.getElementById('btn');
-// btn.onclick = function(e){
-//     e.preventDefault();
-//     alert('asd')
-//     var txt =  document.getElementById("txtCopy").value;
-//     console.log(txt)
-//     document.getElementById("txtPaste").innerText = txt ;
-// }
-
 
 var arrayOfForm = [];
 
@@ -28,15 +17,11 @@ arrayOfForm[0] = {
     Txt: ""
 }
 
-btn.onclick = function fx(){
+
+btn.addEventListener('click',(event) =>{
+    event.preventDefault();
     var noteTitle = document.getElementById("user-title").value;
     var noteText = document.getElementById("user-text").value;
-    console.log(noteTitle)
-    console.log(noteText)
-    arrayOfForm[0].Ttl = noteTitle;
-    arrayOfForm[0].Txt = noteText;
-    console.log(arrayOfForm[0]);
-}
-
-
-btn.addEventListener('click', fx.bind(this));
+    document.getElementById('sticer-title').innerText = noteTitle;
+    document.getElementById('sticer-text').innerText = noteText;
+});
